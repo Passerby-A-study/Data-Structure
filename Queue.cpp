af7f0082b8c5cd0f89c8e389d1fe5bc87cb77f41
Passerby_A <780551528@qq.com>
2025-05-05 16:31:53 +0800

    循环队列实现 2025-05-05 16：31

--- Queue.cpp
+++ Queue.cpp
@@ -86,7 +86,15 @@ Status SqQueue<Type>::ClearQueue()
 	Type* used = Location;
 	Location = nullptr;
 	delete[] used;
-	SqQueue();
+	try
+	{
+		InitQueue();
+	}
+	catch (const char* s)
+	{
+		std::cout << s << std::endl;
+	}
+
 	if (Location && Front == 0 && Rear == 0 && Len == 0)
 	{
 		return OK;
@@ -103,7 +111,7 @@ Status SqQueue<Type>::QueueTraverse()
 	if (QueueEmpty())
 	{
 		std::cout << std::endl;
-		return OK;
+		return ERROR;
 	}
 
 	int index = Front;
@@ -113,6 +121,8 @@ Status SqQueue<Type>::QueueTraverse()
 		index = (index + 1) % SIZE;
 	}
 	std::cout << std::endl;
+
+	return OK;
 
 }
 
@@ -142,8 +152,8 @@ Status SqQueue<Type>::EnQueue(Type& Obj)
 		return ERROR;
 	}
 
-	Rear = (Rear + 1) % SIZE;
 	Location[Rear] = Obj;
+	Rear = (Rear + 1) % SIZE;
 	Len++;
 
 	return OK;
@@ -158,6 +168,84 @@ Status SqQueue<Type>::DeQueue()
 	}
 
 	Front = (Front + 1) % SIZE;
-
+	Len--;
 	return OK;
+}
+
+
+
+int main()
+{
+	SqQueue<int> IntQueue;
+	int choice;
+	int value;
+
+	do {
+		std::cout << "\nint队列操作菜单：" << std::endl;
+		std::cout << "1. 入队" << std::endl;
+		std::cout << "2. 出队" << std::endl;
+		std::cout << "3. 遍历队列" << std::endl;
+		std::cout << "4. 清空队列" << std::endl;
+		std::cout << "5. 获取队列长度" << std::endl;
+		std::cout << "6. 获取队首元素" << std::endl;
+		std::cout << "0. 退出" << std::endl;
+		std::cout << "请输入你的选择: ";
+		std::cin >> choice;
+
+		switch (choice) {
+		case 1:
+			std::cout << "请输入要入队的元素: ";
+			std::cin >> value;
+			if (IntQueue.EnQueue(value) == OK) {
+				std::cout << "入队成功！" << std::endl;
+			}
+			else {
+				std::cout << "队列已满，入队失败！" << std::endl;
+			}
+			break;
+		case 2:
+			if (IntQueue.DeQueue() == OK) {
+				std::cout << "出队成功！" << std::endl;
+			}
+			else {
+				std::cout << "队列为空，出队失败！" << std::endl;
+			}
+			break;
+		case 3:
+			if (IntQueue.QueueLength() == 0)
+			{
+				std::cout << "队列无元素！" << std::endl;
+				break;
+			}
+			std::cout << "队列元素为: ";
+			IntQueue.QueueTraverse();
+			break;
+		case 4:
+			if (IntQueue.ClearQueue() == OK) {
+				std::cout << "队列清空成功！" << std::endl;
+			}
+			else {
+				std::cout << "队列清空失败！" << std::endl;
+			}
+			break;
+		case 5:
+			std::cout << "队列长度为: " << IntQueue.QueueLength() << std::endl;
+			break;
+		case 6:
+			if (!IntQueue.QueueEmpty()) {
+				std::cout << "队首元素为: " << IntQueue.GetHead() << std::endl;
+			}
+			else {
+				std::cout << "队列为空，没有队首元素！" << std::endl;
+			}
+			break;
+		case 0:
+			std::cout << "退出程序。" << std::endl;
+			break;
+		default:
+			std::cout << "无效的选择，请重新输入！" << std::endl;
+		}
+	} while (choice != 0);
+
+	return 0;
 }
